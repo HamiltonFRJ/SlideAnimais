099acf77ad7cb35ddcd2e17db6a7b2a1a92dd458
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-11-17 22:53:49 -0300

    atualizar-distancias

--- js/script.js
+++ js/script.js
@@ -1,32 +1,4 @@
-export default class Slide {
-  constructor(slide, wrapper) {
-    this.slide = document.querySelector(slide);
-    this.wrapper = document.querySelector(wrapper);
-  }
+import Slide from "./slide.js";
 
-  onStart(event) {
-    event.preventDefault();
-  }
-
-  onMove(event) {}
-
-  onEnd(event) {
-    this.wrapper.removeEventListener("mousemove", this.onMove);
-  }
-
-  addSlideEvents() {
-    this.wrapper.addEventListener("mousedown", this.onStart);
-    this.wrapper.addEventListener("mouseup", this.onEnd);
-  }
-
-  bindEvents() {
-    this.onStart = this.onStart.bind(this);
-    this.onMove = this.onMove.bind(this);
-    this.onEnd = this.onEnd.bind(this);
-  }
-
-  init() {
-    this.addSlideEvents();
-    return this;
-  }
-}
+const slide = new Slide(".slide", ".slide-wrapper");
+slide.init();
