293d5d2b47686048f348d043cb787069ce155ead
HamiltonFRJ <hamilton.franc50@gmail.com>
2022-11-22 12:44:47 -0300

    custom-controls-estilisados

--- js/script.js
+++ js/script.js
@@ -3,3 +3,4 @@ import { Slide, SlideNav } from "./slide.js";
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
+slide.addControl(".custom-controls");
